f98b08ebbd4a1df55e9353f57df3e14737825633
Candle779 <Candle77779@gmail.com>
2020-08-02 13:33:32 +0300

    Updates

---
+++ precache-manifest.59b2b7bd3d8ca368fb11bc387dcdcaf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473b34c3b9fe408bb9b3fe596df6e78b",
+    "url": "/yalantis-react-school/index.html"
+  },
+  {
+    "revision": "09a8b23abf8001af8383",
+    "url": "/yalantis-react-school/static/css/main.068d76d3.chunk.css"
+  },
+  {
+    "revision": "490a24599a95d9f0fb2e",
+    "url": "/yalantis-react-school/static/js/2.df883506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yalantis-react-school/static/js/2.df883506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a8b23abf8001af8383",
+    "url": "/yalantis-react-school/static/js/main.3dfed6c1.chunk.js"
+  },
+  {
+    "revision": "4694168ea20fb91eebe2",
+    "url": "/yalantis-react-school/static/js/runtime-main.15bbe14b.js"
+  }
+]);
